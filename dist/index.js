80982faf9a8b377f185ee0034fd459d6d163fbfe
Michael Voke <gadocer54@gmail.com>
2023-06-06 12:53:50 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const user = ["mosh", 1];
+let Msize = 3;
+console.log(Msize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+// let john: number = 14;
+// if (john < 50) john += 10;
+
+// console.log(john);
+
+// const user: [string, number] = ["mosh", 1];
+
+// const enum Size {
+//   small = 1,
+//   medium = 2,
+//   large,
+// }
+
+// let Msize: Size = Size.large;
+
+// console.log(Msize);
+
+// function calculateTax(income: number, taxyear = 2023): number {
+//   if (taxyear < 50_000) return income * 1.6;
+//   return income * 1.3;
+// }
+
+// calculateTax(10_000, 2022);
+
+let employee: {
+  id: number;
+  name: string;
+} = {
+  id: 1,
+  name: "Voke",
+};
