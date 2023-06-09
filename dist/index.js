3a715ab5eca1938171d453697e9895ddd1381cd6
Michael Voke <gadocer54@gmail.com>
2023-06-09 20:05:39 +0100

    Changes to be committed: 	modified:   dist/index.js 	modified:   dist/index.js.map 	modified:   src/index.ts

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 function getCustomer(id) {
     return id === 0 ? null : { birthday: new Date() };
 }
-let customer = getCustomer(0);
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday.getHours());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,6 @@ function getCustomer(id: number): Customer | null {
   return id === 0 ? null : { birthday: new Date() };
 }
 
-let customer = getCustomer(0);
+let customer = getCustomer(1);
 
-console.log(customer?.birthday);
+console.log(customer?.birthday.getHours());
